70779fe4560dc3cdc1f495800678b6c01b09d378
MartaGBayona <marta.g.bayona@outlook.es>
2024-05-30 19:20:09 +0200

    update: add dropdown to header menu

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,17 @@
 "use strict";
+console.log('Antes de DOMContentLoaded');
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('El DOM ha sido cargado');
     const dropdown = document.querySelector('.dropdown');
     const dropdownContent = document.querySelector('.dropdown-content');
+    console.log('Dropdown:', dropdown);
+    console.log('Dropdown Content:', dropdownContent);
     dropdown === null || dropdown === void 0 ? void 0 : dropdown.addEventListener('mouseenter', () => {
+        console.log('El ratón ha entrado en el dropdown');
         dropdownContent === null || dropdownContent === void 0 ? void 0 : dropdownContent.classList.add('show');
     });
     dropdown === null || dropdown === void 0 ? void 0 : dropdown.addEventListener('mouseleave', () => {
+        console.log('El ratón ha salido del dropdown');
         dropdownContent === null || dropdownContent === void 0 ? void 0 : dropdownContent.classList.remove('show');
     });
 });
--- ts/main.ts
+++ ts/main.ts
@@ -1,12 +1,18 @@
+console.log('Antes de DOMContentLoaded');
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('El DOM ha sido cargado');
     const dropdown = document.querySelector('.dropdown');
     const dropdownContent = document.querySelector('.dropdown-content');
+    console.log('Dropdown:', dropdown);
+    console.log('Dropdown Content:', dropdownContent);
     
     dropdown?.addEventListener('mouseenter', () => {
+        console.log('El ratón ha entrado en el dropdown');
         dropdownContent?.classList.add('show');
     });
 
     dropdown?.addEventListener('mouseleave', () => {
+        console.log('El ratón ha salido del dropdown');
         dropdownContent?.classList.remove('show');
     });
 });
